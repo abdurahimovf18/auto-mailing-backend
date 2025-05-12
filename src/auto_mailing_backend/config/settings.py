8d7c422a29cd32eaa6fee923c35a228b12dedf64
Fazliddin Abdurahimov <fazliddin1801@gmail.com>
2025-05-12 18:48:53 +0500

    add(db): alembic has become working and db is connected

--- src/auto_mailing_backend/config/settings.py
+++ src/auto_mailing_backend/config/settings.py
@@ -6,8 +6,24 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 
 
 class Env(BaseSettings):
+    # ---------------------------------------------
+    # Postgresql
+    # ---------------------------------------------
 
+    POSTGRESQL_USER: str
+    POSTGRESQL_PASSWORD: str
+    POSTGRESQL_HOST: str
+    POSTGRESQL_PORT: int
+    POSTGRESQL_DATABASE: str
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env"
     )
+
+
+env = Env()
+
+
+ASYNC_DATABASE_URL = f"postgresql+asyncpg://{env.POSTGRESQL_USER}:{env.POSTGRESQL_PASSWORD}@{env.POSTGRESQL_HOST}:{env.POSTGRESQL_PORT}/{env.POSTGRESQL_DATABASE}"
+SYNC_DATABASE_URL = f"postgresql+psycopg2://{env.POSTGRESQL_USER}:{env.POSTGRESQL_PASSWORD}@{env.POSTGRESQL_HOST}:{env.POSTGRESQL_PORT}/{env.POSTGRESQL_DATABASE}"
+
---
+++ src/auto_mailing_backend/infrastructure/db.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import MetaData
+
+from src.auto_mailing_backend.config.settings import ASYNC_DATABASE_URL
+
+
+
+engine = create_async_engine(
+    ASYNC_DATABASE_URL,
+    pool_size=20,
+    max_overflow=10, 
+    pool_timeout=30,  
+    pool_recycle=1800,
+)
+
+metadata = MetaData()
+session_factory: sessionmaker[AsyncSession] = sessionmaker(bind=engine, class_=AsyncSession)
+
+Base = declarative_base(metadata=metadata)
